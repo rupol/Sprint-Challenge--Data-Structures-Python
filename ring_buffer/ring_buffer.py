6dda7eaced7bf1482300fdead77f36f432865129
Ruth Poliakon <r.poliakon@gmail.com>
2020-06-28 17:42:06 -0600

    ring_buffer complete - passing all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,35 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.index = 0
 
+    # adds the given element to the buffer
     def append(self, item):
-        pass
+        # if length is at capacity, set item to the item at index (0 at first)
+        if len(self.storage) >= self.capacity:
+            self.storage[self.index] = item
+        # add element to front
+        else:
+            self.storage.append(item)
+        # set index
+        self.index = (self.index + 1) % self.capacity
 
+    # returns all of the elements in the buffer in a list in their given order
     def get(self):
-        pass
+        return self.storage
+
+
+buffer = RingBuffer(5)
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+buffer.append('d')
+buffer.append('e')
+print(buffer.get())
+buffer.append('f')
+print(buffer.get())
+buffer.append('g')
+print(buffer.get())
+buffer.append('h')
+print(buffer.get())
