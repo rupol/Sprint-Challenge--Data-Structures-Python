ff2c23c2b350325eb19c8dfd6acee7abac7b76d9
Ruth Poliakon <r.poliakon@gmail.com>
2020-06-28 19:41:19 -0600

    restructured ring_buffer implementation to be more clear

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,14 +6,21 @@ class RingBuffer:
 
     # adds the given element to the buffer
     def append(self, item):
-        # if length is at capacity, set item to the item at index (0 at first)
-        if len(self.storage) >= self.capacity:
-            self.storage[self.index] = item
-        # if length is below capacity, simply add element to back
-        else:
+        # base case: length is below capacity
+        if len(self.storage) < self.capacity:
+            # add element to end of the list
             self.storage.append(item)
-        # set index
-        self.index = (self.index + 1) % self.capacity
+        # if length is at capacity
+        else:
+            # replace the element at the index (0 at first - oldest item in the list) with the new item
+            self.storage[self.index] = item
+            # increment index
+            self.index += 1
+            # if index is capacity, reset to 0
+            if self.index == self.capacity:
+                self.index = 0
+            # alternatively, can use the following to set index to a number that loops from 0 to capacity - 1
+            # self.index = (self.index + 1) % self.capacity
 
     # returns all of the elements in the buffer in a list in their given order
     def get(self):
